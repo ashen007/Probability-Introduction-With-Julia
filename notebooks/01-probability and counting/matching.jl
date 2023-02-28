e8e80966d26df908a71a2df3ebd31b7f48057e83
ashen007 <asheniranga@gmail.com>
2023-02-28 12:22:19 +0530

    replicate function

--- notebooks/01-probability and counting/matching.jl
+++ notebooks/01-probability and counting/matching.jl
@@ -4,3 +4,28 @@ intro_examples:
 - Author: ashen
 - Date: 2023-02-28
 =#
+
+function sample(n)
+    rand(1:n, n)
+
+end
+
+# can not pass a sum(...) function call as it is
+# becaues julia will evaluate it imidiatly amd function
+# replicate get Int64 value return from the expression
+# so need to pass the sum(...) as a Symbol which is not 
+# evaluate imideatly it waits until explicitly said to do so
+function replicate(n, expression::Expr)
+    r = [eval(expression) for _ in 1:n]
+    r
+
+end
+
+function matching(n)
+
+
+end
+
+n = 100
+
+replicate(10, :(sum(sample(n) .== (1:n)))) .>= 1
